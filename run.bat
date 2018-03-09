739f59fbb6f7a7bb15153c86d60740ef49e0d3ab
ggteven <ggteven@gmail.com>
2018-03-09 16:59:25 +0800

    first stage

---
+++ run.bat
@@ -0,0 +1,37 @@
+﻿rmdir /S /Q tool\vba\Input_temp
+rd /s /Q tool\vba\Input_temp
+
+REM Find OS architecture bit
+SET osBit=%PROCESSOR_ARCHITECTURE%
+SET pdftotextProg="tool\xpdf\bin32\pdftotext.exe"
+If %osBit% NEQ x86 (
+ SET pdftotextProg="tool\xpdf\bin64\pdftotext.exe"
+)
+
+
+mkdir "tool\vba\Input_temp"
+
+
+:run_xpdf
+	REM 判斷目次表數量
+	SET /A count=0     
+	REM 執行 pdf to text
+	for /R %%f in ("Input\*") do ( 
+		%pdftotextProg% -cfg "tool\xpdf\xpdfrc" -enc Big5 "%%f" "tool\vba\Input_temp\%%~nf.txt"
+		SET /A count+=1
+	)
+
+If %count% NEQ 16 (
+	tool\ErrorPrompt.vbs "ERROR: There should be 16 pdf files!"
+	goto exit
+)
+
+:runVba
+	tool\vba\run.vbs
+
+:exit
+REM For prevent accidently unremoved dir
+rem	rmdir /S /Q tool\vba\Input_temp
+rem	rd /s /Q tool\vba\Input_temp   	
+
+	
---
+++ tool/vba/run.vbs
@@ -0,0 +1,27 @@
+Option Explicit
+
+ExcelMacroExample
+
+Sub ExcelMacroExample() 
+
+  Dim xlApp 
+  Dim xlBook 
+  Dim objShell
+  Dim strPath
+  
+  Set xlApp = CreateObject("Excel.Application") 
+  xlApp.Visible = True
+  
+  Set objShell = CreateObject("Wscript.Shell")
+  strPath = objShell.CurrentDirectory
+  Set xlBook = xlApp.Workbooks.Open(strPath & "\tool\vba\Main.xls", 0, False) 
+  xlApp.Run "Clear"
+  xlApp.Run "ParseContentTxt"
+  'xlApp.Run "Main"
+
+  'xlBook.Close False
+  'xlApp.Quit
+  Set xlBook = Nothing 
+  Set xlApp = Nothing 
+  Set ObjShell = Nothing
+End Sub 
